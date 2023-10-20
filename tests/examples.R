5a98ecc23be9d031e9f46ef594111c5833270653
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-20 14:53:53 +0200

    last

--- tests/examples.R
+++ tests/examples.R
@@ -9,22 +9,31 @@ library(data.table) # optional, only for results presentation
 library(magrittr) # optional, for piping with %>%
 
 
-# Simulated sample size
-n <- 1000
-# Ensure reproductibility
-set.seed(123)
+############################ Test code ############################
+
+# Import dataset and prepare it for SurveyOaxaca
+df <- fread("tests/eff-pool-2002-2020.csv")
+df[, group := 0][class == "worker", group := 0][class == "capitalist", group := 1]
+df[, rentsbi := 0][rents >= renthog * 0.1 & rents > 2000, rentsbi := 1]
 
 # Define data object simulating a suvey with sampling weigths (variable w)
 data <- data.frame(
-  y = rnorm(n),
-  x1 = rnorm(n),
-  x2 = rnorm(n),
-  group = ifelse(runif(n) > 0.5, 1, 0),
-  w = round(runif(n, 1, 5))
+  y = df$renthog,
+  x1 = df$rentsbi,
+  x2 = as.numeric(as.factor(df$homeowner)) - 2,
+  group = df$group,
+  weights = df$facine3
 )
 
 # Apply "oaxaca_blinder_svy" function to simulated data
-result <- oaxaca_blinder_svy(y ~ x1 + x2, data = data, group = "group", weights = "w", R = 1000)
+result <- oaxaca_blinder_svy(
+  y ~ x1 + x2,
+  data = data,
+  group = "group",
+  weights = "weights",
+  R = 1000
+)
 
 # Return Oaxaca-Blinder decomposition with bootestraped CI
-print(result)
+
+result %>% print()
