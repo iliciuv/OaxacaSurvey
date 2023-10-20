148ea44ec914571911e3b1925239ccb7fb0ac182
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-20 13:16:33 +0200

    last

--- tests/examples.R
+++ tests/examples.R
@@ -20,7 +20,7 @@ data <- data.frame(
 )
 
 # Apply "oaxaca_blinder_svy" function to simulated data
-result <- oaxaca_blinder_svy(y ~ x1 + x2, data = data, group = "group", weights = "w")
+result <- oaxaca_blinder_svy(y ~ x1 + x2, data = data, group = "group", weights = "w", R = 10)
 
 # Return Oaxaca-Blinder decomposition with bootestraped CI
 print(result)
