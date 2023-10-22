e5b0a339efd5ae4fd1a5ac949f18f61f9e32fa6a
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-22 16:56:40 +0200

    last

--- tests/examples2.R
+++ tests/examples2.R
@@ -1,11 +1,12 @@
-library(survey)
-library(boot)
-library(data.table)
-library(magrittr)
-library(OaxacaSurvey
-)
-####################################################################################
+# Example 2: logistic models with multilevel categorical covariates
+# Real data  from Spain's Encuesta Financiera de Familias (2002-2020)
 
+library(data.table)
+library(OaxacaSurvey)
+library(fastDummies)
+
+
+# Import dataset with multilevel categorical data from a s
 df <- fread("tests/eff-pool-2002-2020.csv")
 df <- df[sv_year == 2020]
 df[, group := 0][class == "worker", group := 0][class == "capitalist", group := 1]
@@ -15,30 +16,37 @@ df$class <- relevel(as.factor(df$class), ref = "self-employed")
 df$bage <- relevel(as.factor(df$bage), ref = "0-34")
 df$inherit <- relevel(as.factor(df$inherit), ref = "Non-inherit")
 df$homeowner <- relevel(as.factor(df$homeowner), ref = "Non-Owner")
-df$riquezafin <- factor(as.logical(df$riquezafin), levels = c(T, F), labels = c("Fin", "NonFin"))
+df$riquezafin <- factor(as.logical(df$riquezafin), levels = c(TRUE, FALSE), labels = c("Fin", "NonFin"))
 total_variables <- c(
   "facine3", "renthog", "renthog1", "bage", "homeowner", "worker", "young", "sex", "class",
   "actreales", "riquezanet", "riquezafin", "educ", "auton", "class",
   "tipo_auton", "direc", "multipr", "useprop", "inherit"
 )
 selected_variables <- c(
-  "riquezanet", "bage", "sex", "educ", "inherit", "homeowner", "riquezafin"
+   "renthog", "bage", "sex", "homeowner", "riquezafin"
 )
 
-# adaption to OaxacaSurvey function
+####################################################################################
+
+# select regressors
 data <- df[, ..selected_variables]
+
+# transform multi-level categories to dummies. IMPORTANT remove both first dummy and selected column to get OB decomposition!
 data <- fastDummies::dummy_cols(data,
-  select_columns = c("bage", "sex", "educ", "inherit", "homeowner", "riquezafin"),
-  remove_first_dummy = FALSE,
+  select_columns = c( "bage", "sex", "homeowner", "riquezafin"),
+  remove_first_dummy = TRUE,
   remove_selected_columns = TRUE
 )
+
+# name the columns as x1....xn for oaxaca-blinder survey
 colnames(data) <- paste0("x", seq_along(colnames(data)))
 length_reg <- length(colnames(data))
 new_formula <- paste("y ~", paste0("x", 1:length_reg, collapse = " + "))
-data <- cbind(y = df$renthog, group = df$group, weights = df$facine3, data)
 
+# compose the final dataset by adding: engenous variable, groups of split and sample weights
+data <- cbind(y = df$rentsbi, group = df$group, weights = df$facine3, data)
 
-
+# finally, test the model
 result2 <- oaxaca_blinder_svy(
   as.formula(new_formula),
   data = data.frame(data),
